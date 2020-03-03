ef008db1cd8b1afe6cc2cb7e6ee34c690d2b6f0b
Jackelyn <jackelyntpy@gmail.com>
2020-03-03 20:31:17 +0800

    First commit - html flask

---
+++ projvers1/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, redirect, url_for, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('login.html')
+
+@app.route('/success/<startloc> <endloc>')
+def success(startloc, endloc):
+    return (f'Start search for {startloc} to {endloc}')
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        start_loc = request.form['start']
+        end_loc = request.form['end']
+        return redirect(url_for('success', startloc = start_loc, endloc = end_loc))
+    else:
+        start_loc = request.args.get('start')
+        end_loc = request.args.get('end')
+        return redirect(url_for('success', startloc = start_loc, endloc = end_loc))
+
+
+
+if __name__ == '__main__':
+    app.run()
